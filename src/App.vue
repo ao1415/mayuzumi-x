9ab7cf063d3fa223c12bd6a5b99a245deedb4a4d
ao1415 <ao14159265@gmail.com>
2022-07-24 19:51:17 +0900

    不要なソースを削除

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,4 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  mounted() {
-    console.debug(this.$router);
-    console.debug(this.$router.getRoutes());
-  },
-});
-</script>
-
 <style></style>
